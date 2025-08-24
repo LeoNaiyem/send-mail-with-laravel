0b958bec664dab908db609ad59c1f159a9aad15b
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-08-24 10:54:31 +0600

    simple welcome mail sent

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\WelcomeMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    //
+    public function welcomeMali(){
+        Mail::to('ri9h4lucrr@xkxkud.com')->send(new WelcomeMail());
+        return "Email Sent successfully!";
+    }
 }
